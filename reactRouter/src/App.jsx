b46d1a144544e212941e52d89998e19b212d52b4
Harshal Parab <harshalparab@gmail.com>
2023-09-23 01:17:44 +0530

    router practice commit

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-500 p-4'>React Router</h1>
+      
     </>
   )
 }
