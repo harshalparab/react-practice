5cad86abfb6083295890642e3b38537df5ee7b00
Harshal Parab <harshalparab@gmail.com>
2023-09-24 00:40:48 +0530

    changes for practice contextApi added

--- reactContextApi/src/App.jsx
+++ reactContextApi/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login/Login'
+import Profile from './components/Profile/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <UserContextProvider>
+      <h1>React Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
